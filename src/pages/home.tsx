604ceb13323de22dfe7c840cf306f130b3e26ad9
Umedzhon Izbasarov <umedzhonizbasarov@gmail.com>
2024-07-07 23:10:52 +0500

    title fix

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
   return (
     <>
       <Helmet>
-        <title> Minimal: The starting point for your next project</title>
+        <title> Dunyo Cargo</title>
       </Helmet>
 
       <HomeView />
